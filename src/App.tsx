672c510ca809bdf5f38c826215475f0d12a7dd5d
ChristoferMendes <christoferluizdsm@gmail.com>
2022-09-13 14:55:39 -0300

    tailwind configured

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './styles/main.css'
+
+
+function App() {
+  return(
+    <div className='w-8 h-8 bg-violet-500'>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default App
